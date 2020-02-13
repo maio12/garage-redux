3faa878fd1f193e219092244f3ab25a4d1e4fd0c
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2020-02-13 12:30:10 +0100

    show dummy data-index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 // TODO: add and export your own actions
+export const FETCH_CARS = 'FETCH_CARS';
+
+export function fetchCars() {
+    // const promise = fetch("https://wagon-garage-api.herokuapp.com/antonio/cars")
+    //     .then(response => response.json());
+
+    return {
+        type: FETCH_CARS,
+        payload: cars
+    }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -6,21 +6,38 @@ import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
+import carsReducer from './reducers/car_reducer';
+import CarsIndex from './containers/cars_index';
 
 import '../assets/stylesheets/application.scss';
 
+const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+
+const initialState = {
+  //garageName: garageName,
+  cars: [
+  { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+  { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+  { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+  { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+]
+};
+
 const reducers = combineReducers({
-  // key: reducer
+  //garage: garageReducer,
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
+
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <Router history={history}>
       <Switch>
-        TODO
+        <Route path="/" exact component={CarsIndex} />
       </Switch>
     </Router>
   </Provider>,
